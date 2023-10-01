971846cddfc33298e3a8bf930ce9b906e0680f88
Byron Gracia <byron.gracia@usach.cl>
2023-10-01 15:53:38 -0300

    [Byron Gracia] new InstitucionController para desacoplar codigo

---
+++ src/backend/src/main/java/cl/tbd/backend/controllers/InstitucionController.java
@@ -0,0 +1,46 @@
+package cl.tbd.backend.controllers;
+
+import cl.tbd.backend.models.Institucion;
+import cl.tbd.backend.services.InstitucionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class InstitucionController {
+
+    private final InstitucionService institucionService;
+
+    @Autowired
+    public InstitucionController(InstitucionService institucionService) {
+        this.institucionService = institucionService;
+    }
+
+    @GetMapping("/instituciones")
+    public List<Institucion> getAllInstituciones() {
+        return institucionService.getAllInstituciones();
+    }
+
+    @GetMapping("/institucion/{id}")
+    public Institucion getInstitucionById(@PathVariable Long id) {
+        return institucionService.getInstitucionById(id);
+    }
+
+    @PostMapping("/institucion")
+    public void createInstitucion(@RequestBody Institucion institucion) {
+        institucionService.createInstitucion(institucion);
+    }
+
+    @PutMapping("/institucion/{id}")
+    public void updateInstitucion(@PathVariable Long id, @RequestBody Institucion institucion) {
+        institucion.setIdInstitucion(id);
+        institucionService.updateInstitucion(id,institucion);
+    }
+
+    @DeleteMapping("/institucion/{id}")
+    public void deleteInstitucion(@PathVariable Long id) {
+        institucionService.deleteInstitucion(id);
+    }
+}
--- src/backend/src/main/java/cl/tbd/backend/services/InstitucionService.java
+++ src/backend/src/main/java/cl/tbd/backend/services/InstitucionService.java
@@ -2,42 +2,36 @@ package cl.tbd.backend.services;
 
 import cl.tbd.backend.models.Institucion;
 import cl.tbd.backend.repositories.InstitucionRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RestController
+@Service
 public class InstitucionService {
     private final InstitucionRepository institucionRepository;
 
-    InstitucionService(InstitucionRepository institucionRepository){
+    public InstitucionService(InstitucionRepository institucionRepository) {
         this.institucionRepository = institucionRepository;
     }
 
-    @GetMapping("/instituciones")
-    public List<Institucion> getAllInstituciones(){
+    public List<Institucion> getAllInstituciones() {
         return institucionRepository.getAllInstituciones();
     }
 
-    @GetMapping("/institucion/{id}")
-    public Institucion getInstitucionById(@PathVariable Long id) {
+    public Institucion getInstitucionById(Long id) {
         return institucionRepository.getInstitucionById(id);
     }
 
-    @PostMapping("/institucion")
-    public void createInstitucion(@RequestBody Institucion institucion) {
+    public void createInstitucion(Institucion institucion) {
         institucionRepository.createInstitucion(institucion);
     }
 
-    @PutMapping("/institucion/{id}")
-    public void updateInstitucion(@PathVariable Long id, @RequestBody Institucion institucion) {
+    public void updateInstitucion(Long id, Institucion institucion) {
         institucion.setIdInstitucion(id);
         institucionRepository.updateInstitucion(institucion);
     }
 
-    @DeleteMapping("/institucion/{id}")
-    public void deleteInstitucion(@PathVariable Long id) {
+    public void deleteInstitucion(Long id) {
         institucionRepository.deleteInstitucion(id);
     }
-
 }
