c4b374240fbdb7da4b3e6d839213d1a0759a697e
Orlando Solís Domínguez <orlando.solis.d@usach.cl>
2023-10-17 16:29:34 -0300

    [Orlando Solís] - Delete and move method controller functional

--- src/backend/src/main/java/cl/tbd/backend/services/VoluntarioService.java
+++ src/backend/src/main/java/cl/tbd/backend/services/VoluntarioService.java
@@ -3,10 +3,18 @@ package cl.tbd.backend.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import cl.tbd.backend.models.Voluntario;
 import cl.tbd.backend.repositories.VoluntarioRepository;
 
+@RestController
 @Service
 public class VoluntarioService {
     private final VoluntarioRepository voluntarioRepository;
@@ -15,24 +23,33 @@ public class VoluntarioService {
         this.voluntarioRepository = voluntarioRepository;
     }
 
+    // GET/READ
+    @GetMapping("/voluntarios")
     public List<Voluntario> getAllVoluntarios() {
         return voluntarioRepository.getAllVoluntarios();
     }
 
-    public Voluntario getVoluntarioById(long id) {
+    @GetMapping("/voluntario/{id}")
+    public Voluntario getVoluntarioById(@PathVariable long id) {
         return voluntarioRepository.getVoluntarioById(id);
     }
 
-    public void createVoluntario(Voluntario voluntario) {
+    // CREATE
+    @PostMapping("/voluntario")
+    public void createVoluntario(@RequestBody Voluntario voluntario) {
         voluntarioRepository.createVoluntario(voluntario);
     }
 
-    public void updateVoluntario(Long id, Voluntario voluntario) {
+    // UPDATE
+    @PutMapping("/voluntario/{id}")
+    public void updateVoluntario(@PathVariable Long id, @RequestBody Voluntario voluntario) {
         voluntario.setIdVoluntario(id);
         voluntarioRepository.updateVoluntario(voluntario);
     }
 
-    public void deleteVoluntario(Long id) {
+    // DELETE
+    @DeleteMapping("/voluntario/{id}")
+    public void deleteVoluntario(@PathVariable Long id) {
         voluntarioRepository.deleteVoluntario(id);
     }
 }
