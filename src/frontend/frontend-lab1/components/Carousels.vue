6d5c5864c9d3ab4faba64203c44687fed357fa75
jyr <jairo.santi@usach.cl>
2023-10-22 19:15:41 -0300

    [jairo satni]-Fusion de los Carousel

---
+++ src/frontend/frontend-lab1/components/Carousels.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--  Carousel del Login  -->
+  <v-carousel cycle>
+    <v-carousel-item
+      v-for="(img,i) in imgs"
+      :key="i"
+      :src="img.src"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      imgs: {
+        type: Object,
+        required: true,
+      }
+    },
+  }
+</script>
